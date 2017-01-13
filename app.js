269f46c8ee7dc4c3597c649a04aff9c7ab803126
Francesco Agnoletto <Francesco.agnoletto92@gmail.com>
2017-01-13 12:42:28 +0100

    add passport initialize #2

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
-app.use(passport.initialize());
-app.use(passport.session());
-
 // global config
 var app = express();
 var port = process.env.PORT || 3000;
@@ -16,7 +13,11 @@ app.use('/assets', express.static(__dirname + '/assets'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
 app.use(require('body-parser').urlencoded({ extended: true }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 var Account = require('./models/account');
 passport.use(new LocalStrategy(Account.authenticate()));
