1ec92230aa1efff254eb472ccf2a5177e389ea7a
Francesco Agnoletto <Francesco.agnoletto92@gmail.com>
2017-01-13 12:39:17 +0100

    add passport initialize

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 // global config
 var app = express();
 var port = process.env.PORT || 3000;
