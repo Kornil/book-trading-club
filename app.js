75528ab5d2ddc4cea96ffbdfa4306364c2fdbe95
Francesco Agnoletto <Francesco.agnoletto92@gmail.com>
2017-01-14 00:29:58 +0100

    login bugfix test #6

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.set('view engine', 'ejs');
 
 
 app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
 
 app.use(passport.initialize());
 app.use(passport.session());
